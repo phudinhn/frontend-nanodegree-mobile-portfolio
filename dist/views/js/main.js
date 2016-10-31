3770b8d1d3ed9c290eea157ca277ece12dcbd6e0
Derek <derek@dadian-smith.net>
2016-10-30 23:15:42 -0500

    Optimize query selector
    Move movingPizzas1 query outside of for loop and use getElementById
    instead of querySelector.

--- dist/views/js/main.js
+++ dist/views/js/main.js
@@ -530,6 +530,9 @@ window.addEventListener('scroll', updatePositions);
 document.addEventListener('DOMContentLoaded', function() {
   var cols = 8;
   var s = 256;
+  // Move DOM query out of for loop, and use getElementById instead of querySelector
+  var movingPizzas = document.getElementById("movingPizzas1");
+  
   for (var i = 0; i < 200; i++) {
     var elem = document.createElement('img');
     elem.className = 'mover';
@@ -538,7 +541,7 @@ document.addEventListener('DOMContentLoaded', function() {
     elem.style.width = "73.333px";
     elem.basicLeft = (i % cols) * s;
     elem.style.top = (Math.floor(i / cols) * s) + 'px';
-    document.querySelector("#movingPizzas1").appendChild(elem);
+    movingPizzas.appendChild(elem);
   }
   updatePositions();
 });
